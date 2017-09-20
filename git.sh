395c8f63470efccddbe7e876168edbc7ebc6d9d1
Jake Wright <jake@jakewright.net>
2017-09-20 17:36:00 +0100

    Add gc as an alias for git commit

--- git.sh
+++ git.sh
@@ -1,13 +1,15 @@
 # General git helpers
 
-alias rebasem="git fetch --all; git rebase origin/master"
 alias ga="git add ."
 alias gb="git branch"
+alias gc="git commit"
 alias gd="git diff"
 alias gl="git log"
-alias gs="git status"
 alias gp="git push"
 
 # The --force-with-lease option is like --force
 # but will not overwrite other people's changes.
 alias gpf="git push --force-with-lease"
+
+alias gs="git status"
+alias rebasem="git fetch --all; git rebase origin/master"
